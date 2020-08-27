e214744f2c1885b12b38987ff893155a7c9a9d66
UnikMask <visorunik@gmail.com>
2020-08-27 22:09:03 +0100

    Test upto ivec multiplication.

--- src/test/run_tests.cpp
+++ src/test/run_tests.cpp
@@ -9,12 +9,12 @@ TEST_CASE("vectors can be used for simple vectorial arithmetics", "[int]")
 	{
 		auto test_ptr = std::make_unique<ivec>(0, 0);
 	}
-	ivec a (5, 5);
+	ivec a (7, 3);
 	SECTION("Vectors can be defined and manipulated")
 	{
-		int arr[2] = {7, 3};
+		int arr[2] = {5, 5};
 		gvec_assign(&a, arr);
-		ivec c (7, 3);
+		ivec c (5, 5);
 		REQUIRE(gvec_eq(a, c) == true);
 	}
 	gvec<int> b (2, 4);
@@ -22,8 +22,14 @@ TEST_CASE("vectors can be used for simple vectorial arithmetics", "[int]")
 	{
 		gvec<int> add = a + b; gvec<int> add_res (9, 7);
 		gvec<int> sub = a - b; gvec<int> sub_res (5, -1);
-		REQUIRE(add.num_arr[0] == 9);
 		REQUIRE(gvec_eq(add, add_res) == true);
 		REQUIRE(gvec_eq(sub, sub_res) == true);
+	}
+	SECTION("Vectors can do scalar multiplication (dot product) and by-element multiplication")
+	{
+		gvec<int> bemult = gvec_bemult(a, b); gvec<int> bemult_res (14, 12); 
+		int dot = a * b; int dot_res = 14 + 12;
+		REQUIRE(gvec_eq(bemult, bemult_res) == true);
+		REQUIRE(dot == dot_res);
 	}
 }
