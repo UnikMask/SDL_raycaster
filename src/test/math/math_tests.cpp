2422cc6fe12f8aa4804e6d9ef703c89a0c90b03f
UnikMask <visorunik@gmail.com>
2020-08-27 10:31:31 +0100

    Added temporary tests

--- src/test/math/math_tests.cpp
+++ src/test/math/math_tests.cpp
@@ -1,7 +1,22 @@
-#include "catch2/catch.hpp"
-#include "gvec.h"
-#include "gmat.h"
+#include <catch2/catch.hpp>
+#include "libraycaster.h"
 
 TEST_CASE("Vectors can be used for simple vectorial arithmetics", "[vector]")
 {
+	SECTION("Integer vector arithmetics")
+	{
+		ivec a  = {3, 7}; ivec b  = {2, 21};
+		// Testing vectors
+		int add [2] = {5, 28}; // Addition vector
+		int sub [2] = {1, -14}; // Substraction vector
+		int mult [2] = {9, 21}; // Multiplication vector
+		int bemult [2] = {6, 147}; // per-element multiplication vector
+		int dot = 153; // Dot product
+		REQUIRE(gvec_add(a, b) == true);
+		// REQUIRE((a + b).num_arr == add);
+		//REQUIRE((a - b).num_arr == sub);
+		//REQUIRE(gvec_bemult(a, b).num_arr == bemult);
+		//REQUIRE(a * b == dot);
+		//REQUIRE((a * 3).num_arr == mult);
+	}
 }
--- src/test/run_tests.cpp
+++ src/test/run_tests.cpp
@@ -1,2 +1,4 @@
 #define CATCH_CONFIG_MAIN
 #include "catch.hpp"
+
+// This file is left empty by default and all tests are written in child files.
