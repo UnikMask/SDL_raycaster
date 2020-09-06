a6f469abebbdda294180b8cf14eb417d40d4bfc9
UnikMask <visorunik@gmail.com>
2020-09-06 16:33:55 +0100

    Added gmat template struct + testing for simple additions.

--- include/gmat.h
+++ include/gmat.h
@@ -21,6 +21,7 @@ template <class T> struct gmat
 	gmat<T> operator = (T a[2][2]);
 	gmat<T> (T m00, T m01, T m10, T m11);
 	gmat<T> (gvec<T> row0, gvec<T> row1);
+	gmat<T> (void);
 };
 
 
@@ -31,23 +32,185 @@ typedef struct gmat<int> imat;
 template<class T> gvec<T> operator * (struct gvec<T> a, struct gmat<T> b);
 
 /* Matrix to matrix addition */
-template <class T> gmat<T> gmat_add(struct gmat<T> a, struct gmat<T> b);
+template <class T> struct gmat<T> gmat_add(struct gmat<T> a, struct gmat<T> b)
+{
+	struct gmat<T> c;
+	// Loop on multiplied lengths of num_arr's rows and columns.
+	for (size_t row = 0; row < sizeof(c.num_arr) / sizeof(c.num_arr[0]); row++)
+		for (size_t col = 0; col < sizeof(c.num_arr[0]) / sizeof(T); col++)
+		{
+			c.num_arr[row][col] = a.num_arr[row][col] + b.num_arr[row][col];
+		}
+	return  c;
+}
 /* Matrix to matrix substraction */
-template <class T> gmat<T> gmat_sub(struct gmat<T> a, struct gmat<T> b);
+template <class T> struct gmat<T> gmat_sub(struct gmat<T> a, struct gmat<T> b)
+{
+	struct gmat<T> c;
+	// Loop on multiplied lengths of num_arr's rows and columns.
+	for (size_t row = 0; row < sizeof(c.num_arr) / sizeof(c.num_arr[0]); row++)
+		for (size_t col = 0; col < sizeof(c.num_arr[0]) / sizeof(T); col++)
+		{
+			c.num_arr[row][col] = a.num_arr[row][col] - b.num_arr[row][col];
+		}
+	return  c;
+}
 /* Matrix to constant addition */
-template <class T> gmat<T> gmat_add(struct gmat<T> a, T b);
+template <class T> struct gmat<T> gmat_add(struct gmat<T> a, T b)
+{
+	struct gmat<T> c;
+	// Loop on multiplied lengths of num_arr's rows and columns.
+	for (size_t row = 0; row < sizeof(c.num_arr) / sizeof(c.num_arr[0]); row++)
+		for (size_t col = 0; col < sizeof(c.num_arr[0]) / sizeof(T); col++)
+		{
+			c.num_arr[row][col] = a.num_arr[row][col] + b;
+		}
+	return  c;
+}
 /* Matrix to constant substraction */
-template <class T> gmat<T> gmat_sub(struct gmat<T> a, T b);
+template <class T> struct gmat<T> gmat_sub(struct gmat<T> a, T b)
+{
+	struct gmat<T> c;
+	// Loop on multiplied lengths of num_arr's rows and columns.
+	for (size_t row = 0; row < sizeof(c.num_arr) / sizeof(c.num_arr[0]); row++)
+		for (size_t col = 0; col < sizeof(c.num_arr[0]) / sizeof(T); col++)
+		{
+			c.num_arr[row][col] = a.num_arr[row][col] - b;
+		}
+	return  c;
+}
 /* Matrix to matrix multiplication */
-template <class T> gmat<T> gmat_mult(struct gmat<T> a, struct gmat<T> b);
+template <class T> struct gmat<T> gmat_mult(struct gmat<T> a, struct gmat<T> b)
+{
+	struct gmat<T> c;
+	// Loop on multiplied lengths of num_arr's rows and columns.
+	for (size_t row = 0; row < sizeof(c.num_arr) / sizeof(c.num_arr[0]); row++)
+		for (size_t col = 0; col < sizeof(c.num_arr[0]) / sizeof(T); col++)
+		{
+			c.num_arr[row][col] = 0;
+			for (int n = 0; n < sizeof(a.num_arr) / sizeof(*a.num_arr); n++) 
+			{
+				c.num_arr[row][col] += a.num_arr[row][n] * b.num_arr[n][col];
+			}
+		}
+	return  c;
+}
 /* Vector to matrix multiplication */
-template <class T> gvec<T> gmat_mult(struct gvec<T> a, struct gmat<T> b);
+template <class T> struct gvec<T> gmat_mult(struct gvec<T> a, struct gmat<T> b)
+{
+	struct gvec<T> c;
+	for (int i = 0; i < sizeof(c.num_arr) / sizeof(T); i++)
+	{
+		for (int n = 0; n < sizeof(a.num_arr) / sizeof(T); n++)
+			c.num_arr[i] += a.num_arr[n] * b.num_arr[n][i];
+	}
+	return c;
+}
 /* Matrix to vector multiplication */
-template <class T> gvec<T> gmat_mult(struct gmat<T> a, struct gvec<T> b);
+template <class T> struct gvec<T> gmat_mult(struct gmat<T> a, struct gvec<T> b)
+{
+	struct gvec<T> c;
+	for (int i = 0; i < sizeof(c.num_arr) / sizeof(T); i++)
+	{
+		c.num_arr[i] = 0;
+		for (int n = 0; n < sizeof(b.num_arr) / sizeof(T); n++)
+			c.num_arr[i] += a.num_arr[i][n] * b.num_arr[n]; 
+	}
+	return c;
+}
 /* Matrix to matrix by-element multiplication */
-template <class T> gmat<T> gmat_bemult(struct gmat<T> a, struct gmat<T> b);
+template <class T> struct gmat<T> gmat_bemult(struct gmat<T> a, struct gmat<T> b)
+{
+	struct gmat<T> c;
+	// Loop on multiplied lengths of num_arr's rows and columns.
+	for (size_t row = 0; row < sizeof(c.num_arr) / sizeof(c.num_arr[0]); row++)
+		for (size_t col = 0; col < sizeof(c.num_arr[0]) / sizeof(T); col++)
+		{
+			c.num_arr[row][col] = a.num_arr[row][col] * b.num_arr[row][col];
+		}
+	return  c;
+}
 /* Matrix equality */
-template <class T> bool gmat_eq(struct gmat<T> a, struct gmat<T> b);
+template <class T> bool gmat_eq(struct gmat<T> a, struct gmat<T> b)
+{
+	return ( a.num_arr[0][0] == b.num_arr[0][0] && a.num_arr[0][1] == b.num_arr[0][1] &&
+			 a.num_arr[1][0] == b.num_arr[1][0] && a.num_arr[1][1] == b.num_arr[1][1]);
+}
+/* Matrix assignment */
+template <class T> void gmat_assign (gmat<T>* a, T new_arr[2][2])
+{
+	for (size_t i = 0; i < 2; i++)
+		for (size_t j = 0; j < 2; j++)
+		{
+			a->num_arr[i][j] = new_arr[i][j];
+		}
+}
 
 
+/* gmat struct methods forward declaration*/
+template <class T> gmat<T>::gmat(T m00, T m01, T m10, T m11)
+{
+	this->num_arr[0][0]=m00;
+	this->num_arr[0][1]=m01;
+	this->num_arr[1][0]=m10;
+	this->num_arr[1][1]=m11;
+}
+template <class T> gmat<T>::gmat(gvec<T> row0, gvec<T> row1)
+{
+	for (size_t i = 0; i < 2; i++)
+	{
+		this->num_arr[0][i] = row0.num_arr[i];
+		this->num_arr[1][i] = row1.num_arr[i];
+	}
+}
+template <class T> gmat<T>::gmat(void)
+{
+	for (size_t i = 0; i < sizeof(this->num_arr) / sizeof(this->num_arr[0]); i++)
+		for (size_t j = 0; j < sizeof(this->num_arr[0]) / sizeof(T); j++)
+		{
+			this->num_arr[i][j] = 0;
+		}
+}
+template <class T> gmat<T> gmat<T>::operator + (gmat<T> a)
+{
+	return gmat_add(*this, a);
+}
+template <class T> gmat<T> gmat<T>::operator - (gmat<T> a)
+{
+	return gmat_sub(*this, a);
+}
+template <class T> gmat<T> gmat<T>::operator + (T a)
+{
+	return gmat_add(*this, a);
+}
+template <class T> gmat<T> gmat<T>::operator - (T a)
+{
+	return gmat_sub(*this, a);
+}
+template <class T> gmat<T> gmat<T>::operator * (gmat<T> a)
+{
+	return gmat_mult(*this, a);
+}
+template <class T> gvec<T> gmat<T>::operator * (gvec<T> a)
+{
+	return gmat_mult(*this, a);
+}
+template <class T> gmat<T> gmat<T>::operator * (T a)
+{
+	return gmat_mult(*this, a);
+}
+template <class T> gvec<T> operator * (struct gvec<T> a, struct gmat<T> b)
+{
+	return gmat_mult(a, b);
+}
+template <class T> bool gmat<T>::operator == (gmat<T> a)
+{
+	return gmat_eq(*this, a);
+}
+template <class T> gmat<T> gmat<T>::operator = (T a[2][2])
+{
+	gmat<T> ret_mat;
+	ret_mat.num_arr = a;
+	return ret_mat;
+}
 #endif
--- src/test/run_tests.cpp
+++ src/test/run_tests.cpp
@@ -3,7 +3,8 @@
 #include "libraycaster.h"
 
 // Math tests
-TEMPLATE_TEST_CASE("vectors can be used for simple vectorial arithmetics", "[vector]", int, double)
+TEMPLATE_TEST_CASE("vectors can be used for simple vectorial arithmetics", "[vector]", 
+		int, double)
 {
 	SECTION ("Check if vector initialization and pointers work")
 	{
@@ -31,5 +32,43 @@ TEMPLATE_TEST_CASE("vectors can be used for simple vectorial arithmetics", "[vec
 		TestType dot = a * b; TestType dot_res = 14 + 12;
 		REQUIRE(gvec_eq(bemult, bemult_res) == true);
 		REQUIRE(dot == dot_res);
+	}
+}
+
+TEMPLATE_TEST_CASE("Matrices can be used for simple matrix and vector arithmetics", "[gmat]",
+		int, double)
+{
+	SECTION("Check if matrices can be initialized and gmat pointers work")
+	{
+		auto test_ptr = std::make_unique<gmat<TestType>>(0,0,0,0);
+		REQUIRE(test_ptr);
+		gvec<TestType> a (0, 0);
+		gvec<TestType> b (0, 0);
+		auto ptr_construct_2 = std::make_unique<gmat<TestType>>(a, b);
+		REQUIRE(ptr_construct_2);
+	}
+	gmat<TestType> a (8, 5, 2, 9);
+	SECTION("Matrices can be modified")
+	{
+		TestType new_arr[2][2] = {{5, 5},{5, 5}};
+		gmat_assign(&a, new_arr);
+		gmat<TestType> assign_res (5, 5, 5, 5);
+		REQUIRE(gmat_eq(a, assign_res));
+		
+	}
+	SECTION("Matrices can be used for matrix additon and substraction")
+	{
+		/* Define all needed vectors and matrices for testing */
+		gmat<TestType> b (3, 7, 12, 1);
+		TestType testconst = 3;
+		gmat<TestType> add_res (11, 12, 14, 10); gmat<TestType> add = a + b;
+		gmat<TestType> sub_res (5, -2, -10, 8); gmat<TestType> sub = a - b;
+		gmat<TestType> constadd_res (11, 8, 5, 12); gmat<TestType> constadd = a + testconst;
+		gmat<TestType> constsub_res (5, 2, -1, 6); gmat<TestType> constsub = a - testconst;
+
+		REQUIRE(gmat_eq(add, add_res));
+		REQUIRE(gmat_eq(sub, sub_res));
+		REQUIRE(gmat_eq(constadd, constadd_res));
+		REQUIRE(gmat_eq(constsub, constsub_res));
 	}
 }
