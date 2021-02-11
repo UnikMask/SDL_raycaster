81b60f2d243d4901b2f51daab437faf65733dc2f
Alexandre Guerville <alexandre.guerville@hotmail.com>
2021-02-11 22:36:33 +0100

    Fixed matrix issues + documented gmat and gvec.

--- include/gvec.h
+++ include/gvec.h
@@ -8,38 +8,54 @@ template <class T>
 struct gvec
 {
 	public:
-	T num_arr[2];
-	gvec<T> operator + (gvec<T> a); // Vector and vector additon
-	gvec<T> operator - (gvec<T> a); // Vector and vector substraction
-	gvec<T> operator + (T a); // Vector and primitive addition {
-	gvec<T> operator - (T a); // Vector and primitive substraction
-	T operator * (gvec<T> a); // Dot product
-	gvec<T> operator * (T a); // Vector and primitive multiplication
-	bool operator == (const gvec<T> a); // Vector to vector Equality
-	bool operator == (const T a[2]); // Vector to array equality
-	gvec<T> (T a, T b); // Constructor
-	gvec<T> ();
+		/// Array of elements of the vector.
+		T num_arr[2];
+		/// Vector and vector additon.
+		gvec<T> operator + (gvec<T> a);
+		/// Vector and vector substraction.
+		gvec<T> operator - (gvec<T> a);
+		/// Vector and primitive addition.
+		gvec<T> operator + (T a);
+		/// Vector and primitive substraction.
+		gvec<T> operator - (T a);
+		/// Dot product.
+		T operator * (gvec<T> a);
+		/// Vector and primitive multiplication.
+		gvec<T> operator * (T a);
+		/// Vector to vector Equality.
+		bool operator == (const gvec<T> a);
+		/// Vector to array equality.
+		bool operator == (const T a[2]);
+		/// Constructor with defined values.
+		gvec<T> (T a, T b);
+		/// Null vector constructor.
+		gvec<T> ();
 };
 
+/// Vector of doubles.
 typedef gvec<double> dvec;
+/// Vector of integers.
 typedef gvec<int> ivec;
 
 
-/* Vector to vector addition */
-template <class T> gvec<T> gvec_add(struct gvec<T> a, struct gvec<T> b)
-{
+/**
+ * Perform vector addition.
+ * @param a 1st vector.
+ * @param b 2nd vector.
+ * @return Resulting addition.
+ */
+template <class T> gvec<T> gvec_add(struct gvec<T> a, struct gvec<T> b) {
 	return gvec<T> (a.num_arr[0] + b.num_arr[0], 
 			a.num_arr[1] + b.num_arr[1]);
 }
-/* Vector to vector substraction */
-template <class T> gvec<T> gvec_sub(struct gvec<T> a, struct gvec<T> b)
-{
-	return gvec<T> (a.num_arr[0] - b.num_arr[0],
-			a.num_arr[1] - b.num_arr[1]);
-}
-/* Vector to raw type addition */
-template <class T> gvec<T> gvec_add(struct gvec<T> a, T b)
-{ 
+
+/**
+ * Perform vector and single number addition.
+ * @param a Vector to add to.
+ * @param b Adding single number.
+ * @return resulting addition.
+ */
+template <class T> gvec<T> gvec_add(struct gvec<T> a, T b) {
 	struct gvec<T> c (0, 0);
 	for (int i = 0; i < sizeof(c.num_arr) / sizeof(*c.num_arr); i++)
 	{
@@ -47,96 +63,107 @@ template <class T> gvec<T> gvec_add(struct gvec<T> a, T b)
 	}
 	return c;
 }
-/* Vector to raw type substraction */
-template <class T> gvec<T> gvec_sub(struct gvec<T> a, T b)
-{
-	struct gvec<T> c (a.num_arr[0], a.num_arr[1]);
-	for (int i = 0; i < sizeof(c.num_arr) / sizeof(*c.num_arr); i++)
-		c.num_arr[i] -= b;
-	return c;
-}
-/* Vector by constant multiplication*/
-template <class T> gvec<T> gvec_mult(struct gvec<T> a, T b)
-{
+
+/**
+ * Multiply vector by a single number.
+ * @param a Vector to multiply.
+ * @param b Number used as factor.
+ * @param resulting product.
+ */
+template <class T> gvec<T> gvec_mult(struct gvec<T> a, T b) {
 	struct gvec<T> c (a.num_arr[0], a.num_arr[1]);
 	for (int i = 0; i < sizeof(c.num_arr) / sizeof(*c.num_arr); i++)
 		c.num_arr[i] *= b;
 	return c;
 }
-/* Vector scalar multiplication */
-template <class T> T gvec_mult(struct gvec<T> a, struct gvec<T> b)
-{
+
+/**
+ * Perform vector dot product.
+ * @param a lhs row vector.
+ * @param b rhs column vector.
+ * @return Resulting dot product.
+ */
+template <class T> T gvec_mult(struct gvec<T> a, struct gvec<T> b) {
 	return a.num_arr[0] * b.num_arr[0] + a.num_arr[1] * b.num_arr[1];
 }
-/* Vector by-element multiplication */
-template <class T> gvec<T> gvec_bemult(struct gvec<T> a, struct gvec<T> b)
-{
+
+/**
+ * Perform by-element vector product.
+ * @param a 1st vector.
+ * @param b 2nd vector.
+ * @return Resulting product.
+ */
+template <class T> gvec<T> gvec_bemult(struct gvec<T> a, struct gvec<T> b) {
 	struct gvec<T> c (a.num_arr[0], a.num_arr[1]);
 	for (int i = 0; i < sizeof(c.num_arr) / sizeof(*c.num_arr); i++)
 		c.num_arr[i] *= b.num_arr[i];
 	return c;
 }
-/* Assignment method for vector */
-template <class T> void gvec_assign (gvec<T> a, gvec<T> b)
-{
-	for (size_t i = 0; i < sizeof(a.num_arr)/sizeof(*a.num_arr); i++)
-	{
+
+/**
+ * Assign value to a vector.
+ * @param a vector to assign to.
+ * @param b vector to copy.
+ */
+template <class T> void gvec_assign (gvec<T> a, gvec<T> b) {
+	for (size_t i = 0; i < sizeof(a.num_arr)/sizeof(*a.num_arr); i++) {
 		a.num_arr[i] = b.num_arr[i];
 	}
 }
-/* Assignment method for vector with an array */
-template <class T> void gvec_assign (gvec<T>* a, T b[2])
-{
+
+/**
+ * Assign array of values to a vector.
+ * @param a Vector to assign to.
+ * @param b Array to assign to vector.
+ */
+template <class T> void gvec_assign (gvec<T>* a, T b[2]) {
 	a->num_arr[0] = b[0];
 	a->num_arr[1] = b[1];
 }
 
-template <class T> bool gvec_eq(gvec<T> a, gvec<T> b) // Vector and vector equality
-{
+/**
+ * Perform vector equality.
+ * @param a 1st vector.
+ * @param b 2nd vector.
+ * @return Equality status.
+ */
+template <class T> bool gvec_eq(gvec<T> a, gvec<T> b) {
 	if (a.num_arr[0] == b.num_arr[0] & a.num_arr[1] == b.num_arr[1]) return true; 
 	else return false;
 }
 
-template <class T> gvec<T> gvec<T>::operator + (gvec<T> a) // Vector and vector additon
-{
+template <class T> gvec<T> gvec<T>::operator + (gvec<T> a)  {
 	return gvec_add(*this, a);
 }
-template <class T> gvec<T> gvec<T>::operator - (gvec<T> a) // Vector and vector substraction
-{
-	return gvec_sub(*this, a);
+template <class T> gvec<T> gvec<T>::operator - (gvec<T> a) {
+	return gvec_add(*this, a * (-1));
 }
-template <class T> gvec<T> gvec<T>::operator + (T a) // Vector and primitive addition {
-{
+template <class T> gvec<T> gvec<T>::operator + (T a) {
 	return gvec_add(*this, a);
 }
-template <class T> gvec<T> gvec<T>::operator - (T a) // Vector and primitive substraction
-{
-	return gvec_sub(*this, a);
+template <class T> gvec<T> gvec<T>::operator - (T a) {
+	return gvec_add(*this, a * (-1));
 }
-template <class T> T gvec<T>::operator * (gvec<T> a) // Dot product
-{
+template <class T> T gvec<T>::operator * (gvec<T> a) {
 	return gvec_mult(*this, a);
 }
-template <class T> gvec<T> gvec<T>::operator * (T a) // Vector and primitive multiplication
-{
+template <class T> gvec<T> gvec<T>::operator * (T a) {
 	return gvec_mult(*this, a);
 }
-template <class T> bool gvec<T>::operator == (const gvec<T> a) // Vector to vector Equality
-{
+template <class T> bool gvec<T>::operator == (const gvec<T> a) {
 	return gvec_eq(*this, a);
 }
-template <class T> bool gvec<T>::operator == (const T a[2]) // Vector to array equality
-{
+template <class T> bool gvec<T>::operator == (const T a[2]) {
 	gvec<T> b (a[0], a[1]);
 	return gvec_eq(*this, b);
 }
-template <class T> gvec<T>::gvec (T a, T b) // Constructor
-{
+template <class T> gvec<T>::gvec (T a, T b) {
 	this->num_arr[0] = a;
 	this->num_arr[1] = b;
 }
-template <class T> gvec<T>::gvec ()
-{
+template <class T> gvec<T>::gvec () {
+	this->num_arr[0] = 0;
+	this->num_arr[1] = 0;
 }
 
 #endif
--- src/test/run_tests.cpp
+++ src/test/run_tests.cpp
@@ -2,32 +2,28 @@
 #include <catch2/catch.hpp>
 #include "libraycaster.h"
 
+
 // Math tests
 TEMPLATE_TEST_CASE("vectors can be used for simple vectorial arithmetics", "[vector]", 
-		int, double)
-{
-	SECTION ("Check if vector initialization and pointers work")
-	{
+		int, double) {
+	SECTION ("Check if vector initialization and pointers work") {
 		auto test_ptr = std::make_unique<gvec<TestType>>(0, 0);
 	}
 	gvec<TestType> a (7, 3);
-	SECTION("Vectors can be defined and manipulated")
-	{
+	SECTION("Vectors can be defined and manipulated") {
 		TestType arr[2] = {5, 5};
 		gvec_assign(&a, arr);
 		gvec<TestType> c (5, 5);
 		REQUIRE(gvec_eq(a, c) == true);
 	}
 	gvec<TestType> b (2, 4);
-	SECTION("Vectors can do additions and substractions")
-	{
+	SECTION("Vectors can do additions and substractions") {
 		gvec<TestType> add = a + b; gvec<TestType> add_res (9, 7);
 		gvec<TestType> sub = a - b; gvec<TestType> sub_res (5, -1);
 		REQUIRE(gvec_eq(add, add_res) == true);
 		REQUIRE(gvec_eq(sub, sub_res) == true);
 	}
-	SECTION("Vectors can do scalar multiplication (dot product) and by-element multiplication")
-	{
+	SECTION("Vectors can do scalar multiplication (dot product) and by-element multiplication") {
 		gvec<TestType> bemult = gvec_bemult(a, b); gvec<TestType> bemult_res (14, 12); 
 		TestType dot = a * b; TestType dot_res = 14 + 12;
 		REQUIRE(gvec_eq(bemult, bemult_res) == true);
@@ -35,64 +31,59 @@ TEMPLATE_TEST_CASE("vectors can be used for simple vectorial arithmetics", "[vec
 	}
 }
 
-TEMPLATE_TEST_CASE("Matrices can be used for simple matrix and vector arithmetics", "[gmat]",
-		int, double)
-{
-	SECTION("Check if matrices can be initialized and gmat pointers work")
-	{
-		auto test_ptr = std::make_unique<gmat<TestType>>(0,0,0,0);
-		REQUIRE(test_ptr);
-		gvec<TestType> a (0, 0);
-		gvec<TestType> b (0, 0);
-		auto ptr_construct_2 = std::make_unique<gmat<TestType>>(a, b);
-		REQUIRE(ptr_construct_2);
-	}
-	gmat<TestType> a (8, 5, 2, 9);
-	SECTION("Matrices can be modified")
-	{
-		TestType new_arr[2][2] = {{5, 5},{5, 5}};
-		gmat_assign(&a, new_arr);
-		gmat<TestType> assign_res (5, 5, 5, 5);
-		REQUIRE(gmat_eq(a, assign_res));
-		
-	}
-	gmat<TestType> b (3, 7, 12, 1);
-	SECTION("Matrices can be used for matrix additon and substraction")
-	{
-		/* Define all needed vectors and matrices for testing */
-		TestType testconst = 3;
-		gmat<TestType> add_res (11, 12, 14, 10); gmat<TestType> add = a + b;
-		gmat<TestType> sub_res (5, -2, -10, 8); gmat<TestType> sub = a - b;
-		gmat<TestType> constadd_res (11, 8, 5, 12); gmat<TestType> constadd = a + testconst;
-		gmat<TestType> constsub_res (5, 2, -1, 6); gmat<TestType> constsub = a - testconst;
+TEMPLATE_TEST_CASE("Matrices can be used for simple matrix and vector arithmetics",
+		"[gmat]", int, double) {
+SECTION("Check if matrices can be initialized and gmat pointers work") {
+auto test_ptr = std::make_unique<gmat<TestType>>(0,0,0,0);
+REQUIRE(test_ptr);
+gvec<TestType> a (0, 0);
+gvec<TestType> b (0, 0);
+auto ptr_construct_2 = std::make_unique<gmat<TestType>>(a, b);
+REQUIRE(ptr_construct_2);
+}
+gmat<TestType> a (8, 5, 2, 9);
+SECTION("Matrices can be modified") {
+TestType new_arr[2][2] = {{5, 5},{5, 5}};
+gmat_assign(&a, new_arr);
+gmat<TestType> assign_res (5, 5, 5, 5);
+REQUIRE(gmat_eq(a, assign_res));
 
-		REQUIRE(gmat_eq(add, add_res));
-		REQUIRE(gmat_eq(sub, sub_res));
-		REQUIRE(gmat_eq(constadd, constadd_res));
-		REQUIRE(gmat_eq(constsub, constsub_res));
-	}
-	SECTION("Matrices can be used for scalar and per-element multiplication and arithmetics")
-	{
-		gmat<TestType> matmat0_res (29, 66, 15, 32);
-			gmat<TestType> matmat0 = a * b;
-		gmat<TestType> matmat1_res (38, 78, 12, 23);
-			gmat<TestType> matmat1 = b * a;
+}
+gmat<TestType> b (3, 7, 12, 1);
+SECTION("Matrices can be used for matrix additon and substraction") {
+/* Define all needed vectors and matrices for testing */
+TestType testconst = 3;
+gmat<TestType> add_res (11, 12, 14, 10); gmat<TestType> add = a + b;
+gmat<TestType> sub_res (5, -2, -10, 8); gmat<TestType> sub = a - b;
+gmat<TestType> constadd_res (11, 8, 5, 12); gmat<TestType> constadd = a + testconst;
+gmat<TestType> constsub_res (5, 2, -1, 6); gmat<TestType> constsub = a - testconst;
 
-		gmat<TestType> bemult_res (24, 35, 2, 18);
-			gmat<TestType> bemult = gmat_bemult(a, b);
+REQUIRE(gmat_eq(add, add_res));
+REQUIRE(gmat_eq(sub, sub_res));
+REQUIRE(gmat_eq(constadd, constadd_res));
+REQUIRE(gmat_eq(constsub, constsub_res));
+}
+SECTION("Matrices can be used for scalar and per-element multiplication and arithmetics") {
+gmat<TestType> matmat0_res (84, 61, 114, 23);
+gmat<TestType> matmat0 = a * b;
+gmat<TestType> matmat1_res (38, 78, 98, 69);
+gmat<TestType> matmat1 = b * a;
 
-		gvec<TestType> c (4, -2); // Initialize vector for rest of tests.
-		gvec<TestType> vecmat_res (22, -10);
-			gvec<TestType> vecmat = a * c;
-		gvec<TestType> matvec_res (28, 2);
-			gvec<TestType> matvec = c * a;
+gmat<TestType> bemult_res (24, 35, 24, 9);
+gmat<TestType> bemult = gmat_bemult(a, b);
 
-		REQUIRE(matmat0.num_arr[0][1] == 66);
-		REQUIRE(gmat_eq(matmat0, matmat0_res));
-		REQUIRE(gmat_eq(matmat1, matmat1_res));
-		REQUIRE(gvec_eq(vecmat, vecmat_res));
-		REQUIRE(gvec_eq(matvec_res, matvec));
-		REQUIRE(gmat_eq(bemult, bemult_res));
-		
-	}
+gvec<TestType> c (4, -2); // Initialize vector for rest of tests.
+gvec<TestType> vecmat_res (22, -10);
+gvec<TestType> vecmat = a * c;
+gvec<TestType> matvec_res (28, 2);
+gvec<TestType> matvec = c * a;
+
+REQUIRE(matmat0.num_arr[0][1] == 61);
+REQUIRE(gmat_eq(matmat0, matmat0_res));
+REQUIRE(gmat_eq(matmat1, matmat1_res));
+REQUIRE(gvec_eq(vecmat, vecmat_res));
+REQUIRE(gvec_eq(matvec_res, matvec));
+REQUIRE(gmat_eq(bemult, bemult_res));
+
+}
 }
